777d9ac30fed50bcb0f1caa9f34fe98058d9b5c7
Archit1706 <architrathod77@gmail.com>
2023-08-31 20:01:07 +0530

    Resume

---
+++ pages/resume/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+const App = () => {
+
+    useEffect(() => {
+        document.title = 'Archit Rathod | Resume';
+        const handleDownload = () => {
+            const fileName = 'Archit_Rathod.pdf';
+            const fileUrl = '/resume/Archit_Rathod.pdf';
+
+            const link = document.createElement('a');
+            link.href = fileUrl;
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.location.href = '/';
+        };
+        handleDownload();
+    }, [])
+    return (
+        <div className='App'>App</div>
+    );
+}
+
+export default App;
