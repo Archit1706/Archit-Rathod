fd969d75fd63072a68c44406da1b4d357900a06e
Archit1706 <architrathod77@gmail.com>
2023-09-26 21:42:18 +0530

    dark mode

--- theme.config.jsx
+++ theme.config.jsx
@@ -11,7 +11,7 @@ export default {
     ),
     readMore: "Read More →",
     postFooter: null,
-    darkMode: false,
+    darkMode: true,
     // navs: [
     //     {
     //         url: "https://github.com/shuding/nextra",
