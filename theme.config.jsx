ad4bcf3dca4002a24bdc154d85817e30ae1c7247
Archit1706 <architrathod77@gmail.com>
2023-10-01 00:16:45 +0530

    try

--- theme.config.jsx
+++ theme.config.jsx
@@ -75,7 +75,7 @@ export default {
             )}
             {meta.tag && <meta name="keywords" content={meta.tag} />}
             {meta.author && <meta name="author" content={meta.author} />}
-            <title>{title} - Archit Blog&apos;s!</title>
+            <title>{title} | Archit Blog&apos;s!</title>
         </>
     ),
     readMore: "Read More →",
