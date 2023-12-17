b710aca49b03db614aa87ec432655a2ff0924de2
Archit1706 <architrathod77@gmail.com>
2023-12-17 15:07:03 +0530

    Research Meta

--- pages/research/[research].js
+++ pages/research/[research].js
@@ -21,7 +21,7 @@ const ResearchDetails = () => {
             <div className="w-full">
                 <div className="max-w-[1240px] w-full h-full mx-auto flex flex-col justify-center items-center">
                     <div className="w-full h-[60vh] relative">
-                        <div className="absolute top-0 left-0 w-full h-[60vh] bg-black/70 z-10" />
+                        <div className="absolute top-0 left-0 w-full h-[60vh] bg-black/60 z-10" />
                         <Image
                             className="absoute z-1"
                             layout="fill"
@@ -31,41 +31,115 @@ const ResearchDetails = () => {
                         />
                         <div className="absolute top-[60%] w-[90%] left-[50%] right=[50%] translate-x-[-50%] translate-y-[-50%] ml-6 text-white z-10">
                             <h2 className="py-2">{researchData.title}</h2>
-                            <h3>
-                                {"| "}
-                                {researchData.authors.map(
-                                    (author) => author + " | "
-                                )}
-                            </h3>
+                            {researchData.status == "published" ? (
+                                <>
+                                    <h3 className="text-lg font-bold tracking-wide text-white">
+                                        <span>Published at </span>
+                                        {researchData.workshop}
+                                    </h3>
+                                    <p className="py-1 text-gray-200 tracking-wide">
+                                        {researchData.conference}
+                                    </p>
+                                </>
+                            ) : researchData.status == "accepted" ? (
+                                <>
+                                    <h3 className="text-lg font-bold tracking-wide text-white">
+                                        <span>Selected at</span>{" "}
+                                        {researchData.workshop}
+                                    </h3>
+                                    <p className="py-1 text-gray-200 tracking-wide">
+                                        {researchData.conference ||
+                                            researchData.organization}
+                                    </p>
+                                </>
+                            ) : (
+                                <>
+                                    <h3 className="text-lg font-bold tracking-wide text-white">
+                                        Working Paper
+                                    </h3>
+                                    <p className="py-1 text-gray-200 tracking-wide">
+                                        {researchData.dates.working}
+                                    </p>
+                                </>
+                            )}
                         </div>
                     </div>
 
                     <AnimationWrapper>
                         <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
                             <div className="col-span-4">
-                                <p>Project</p>
-                                <h2>Overview</h2>
-                                <p>{researchData.desc}</p>
-                                {researchData.link != "" && (
+                                {/* <p>Overview</p> */}
+                                <h3>Abstract</h3>
+                                <p>{researchData.abstract}</p>
+
+                                {researchData.links.code != "" && (
                                     <a
-                                        href={researchData.link}
+                                        href={researchData.links.code}
                                         target="__blank"
                                         rel="noreferrer"
                                     >
                                         <button className="px-8 py-2 mt-4 mr-8">
-                                            Demo
+                                            Code
                                         </button>
                                     </a>
                                 )}
 
-                                {researchData.github != "" && (
+                                {researchData.links.openreview != "" && (
                                     <a
-                                        href={researchData.github}
+                                        href={researchData.links.openreview}
                                         target="__blank"
                                         rel="noreferrer"
                                     >
-                                        <button className="px-8 py-2 mt-4">
-                                            Code
+                                        <button className="px-8 py-2 mt-4 mr-8">
+                                            OpenReview
+                                        </button>
+                                    </a>
+                                )}
+
+                                {researchData.links.arxiv != "" && (
+                                    <a
+                                        href={researchData.links.arxiv}
+                                        target="__blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="px-8 py-2 mt-4 mr-8">
+                                            ArXiv
+                                        </button>
+                                    </a>
+                                )}
+
+                                {researchData.links.pdf != "" && (
+                                    <a
+                                        href={researchData.links.pdf}
+                                        target="__blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="px-8 py-2 mt-4 mr-8">
+                                            PDF
+                                        </button>
+                                    </a>
+                                )}
+
+                                {researchData.links.slides != "" && (
+                                    <a
+                                        href={researchData.links.slides}
+                                        target="__blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="px-8 py-2 mt-4 mr-8">
+                                            Slides
+                                        </button>
+                                    </a>
+                                )}
+
+                                {researchData.links.video != "" && (
+                                    <a
+                                        href={researchData.links.video}
+                                        target="__blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="px-8 py-2 mt-4 mr-8">
+                                            Video
                                         </button>
                                     </a>
                                 )}
